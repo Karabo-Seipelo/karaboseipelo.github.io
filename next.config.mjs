2dd2e46d650aba3d94801500f399845d06f97ba6
Karabo Seipelo <1788640+Karabo-Seipelo@users.noreply.github.com>
2024-10-12 11:12:28 +0200

    re-worked

---
+++ next.config.mjs
@@ -0,0 +1,7 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    output: "export",  // This will create a static site
+    reactStrictMode: true,
+};
+
+export default nextConfig;
