9efe6474793084b869da72ad5f815943956d64c3
Karabo Seipelo <1788640+Karabo-Seipelo@users.noreply.github.com>
2024-10-12 10:53:32 +0200

    removed .next

--- .next/static/Ti3ACKXrIgbd84RrMDFV4/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/css/27b4d6b789fddb24.css","static/chunks/pages/index-c49278c63ab487fa.js"],"/_error":["static/chunks/pages/_error-77823ddac6993d35.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
